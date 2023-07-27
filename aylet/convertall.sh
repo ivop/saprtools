093031390be90cc9c04a164051f7c72dfc4e360d
Ivo van Poorten <ivopvp@gmail.com>
2023-07-27 09:47:25 +0200

    add some speccy and cpc files plus conversion script

---
+++ aylet/convertall.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+set -ex
+
+./aylet -y -t 1 spectrum/BackToTheFuture2.ay > ../ym2sapr/spectrum/BackToTheFuture2.ym
+./aylet -y -t 1 spectrum/BeverlyHillsCop.ay > ../ym2sapr/spectrum/BeverlyHillsCop.ym
+
+./aylet -y -c -t 1 cpc/'twistn shout 1.ay' > ../ym2sapr/cpc/'twistn shout 1.ym'
+./aylet -y -c -t 1 cpc/"Burnin'Rubber.ay" > ../ym2sapr/cpc/"Burnin'Rubber..ym"
+
