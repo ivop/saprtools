550be9e7cc7c492c9093c0a1145a967afa40114a
Ivo van Poorten <ivopvp@gmail.com>
2023-07-28 09:23:00 +0200

    add tim follin song

--- aylet/convertall.sh
+++ aylet/convertall.sh
@@ -4,6 +4,7 @@ set -ex
 
 ./aylet -y -t 1 spectrum/BackToTheFuture2.ay > ../ym2sapr/spectrum/BackToTheFuture2.ym
 ./aylet -y -t 1 spectrum/BeverlyHillsCop.ay > ../ym2sapr/spectrum/BeverlyHillsCop.ym
+./aylet -y -A 56 -F 1 spectrum/MissileGroundZero.AY > ../ym2sapr/spectrum/MissileGroundZero.ym
 
 ./aylet -y -c -t 1 cpc/'twistn shout 1.ay' > ../ym2sapr/cpc/'twistn shout 1.ym'
 ./aylet -y -c -t 1 cpc/"Burnin'Rubber.ay" > ../ym2sapr/cpc/"Burnin'Rubber..ym"
