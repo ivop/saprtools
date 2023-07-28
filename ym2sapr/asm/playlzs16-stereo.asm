409e866c28fa83e690739c8383e95193f38d4c12
Ivo van Poorten <ivopvp@gmail.com>
2023-07-28 09:05:15 +0200

    v1.6

--- ym2sapr/asm/playlzs16-stereo.asm
+++ ym2sapr/asm/playlzs16-stereo.asm
@@ -37,7 +37,7 @@ SKCTL = $d20f
     mva #15 $02c5   ; white foreground
     mva #0 82       ; left margin
     prints 0, " "
-    prints 0, "YM2SAPR v1.5"
+    prints 0, "YM2SAPR v1.6"
     prints 0, "Copyright (C) 2023 by Ivo van Poorten"
     prints 0, "LZSS routines (C) 2020 by DMSC"
     prints 0, " "
