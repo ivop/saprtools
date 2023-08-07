0885efdcd47520bf4a67577073e6d96093683921
Ivo van Poorten <ivopvp@gmail.com>
2023-08-07 06:09:10 +0200

    implement softbass player

--- ym2sapr/main.cpp
+++ ym2sapr/main.cpp
@@ -94,7 +94,7 @@ enum {
 };
 
 const char *basstypes[BASS_COUNT] = {
-    "transpose", "gritty", "buzzy", "softsynth"
+    "transpose", "gritty", "buzzy", "softbass"
 };
 
 enum {
@@ -551,11 +551,11 @@ static void usage(void) {
 "   -d          disable envelopes\n"
 "   -e volume   envelopes as fixed YM volume\n"
 "   -f divider  use envelope frequency / divider as note\n"
-"   -c cloxk    override master clock [default:2000000 or read from file]\n"
+"   -c clock    override master clock [default:2000000 or read from file]\n"
 "   -p volume   override pokey maximum per channel volume [default: 12\n"
 "   -r map      remap channels [default: abc]\n"
 "   -m          eneable mono pokey mode [default: stereo pokey]\n"
-"   -b num      mono bass type (transpose [default], gritty, buzzy or softsynth)\n"
+"   -b type     mono bass type (transpose [default], gritty, buzzy or softbass)\n"
 );
 }
 
