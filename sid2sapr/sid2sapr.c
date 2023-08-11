3523dec7f9a451240a8eb4ddfc71ced764d21582
Ivo van Poorten <ivopvp@gmail.com>
2023-08-11 04:55:40 +0200

    add command line option parsing

--- sid2sapr/sid2sapr.c
+++ sid2sapr/sid2sapr.c
@@ -2,22 +2,83 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 #include "sidengine.h"
 
+#define C64_CLOCK        985248L
+#define ATARI_CLOCK     1773447L
+
 #define NSAMPLES 882                    // 44100/50 = 882
+
+/* ------------------------------------------------------------------------ */
+
 static uint16_t soundbuffer[NSAMPLES*2];
 
 static uint16_t init_addr, play_addr;
 static uint8_t actual_subsong, max_subsong, speed100Hz;
 static char song_name[33], song_author[33], song_copyright[33];
 
-void show_info(int argc) {
-    fprintf(stderr, "usage: sid2sapr sid-file\n"
-);
+#define DEFAULT_MAXPOKVOL 12
+unsigned int maxpokvol = DEFAULT_MAXPOKVOL;
+
+unsigned int mono = 0;
+int basstype = 0;
+char *bassstring;
+
+enum {
+    BASS_TRANSPOSE = 0,
+    BASS_GRITTY,
+    BASS_BUZZY,
+    BASS_SOFTBASS,
+    BASS_COUNT
+};
+
+const char *basstypes[BASS_COUNT] = {
+    "transpose", "gritty", "buzzy", "softbass"
+};
+
+
+/* ------------------------------------------------------------------------ */
+
+void usage(void) {
+    fprintf(stderr, "usage: sid2sapr [-b type] sid-file\n\n"
+"   -b type     bass type (transpose [default], gritty, buzzy or softbass)\n");
 }
+
+/* ------------------------------------------------------------------------ */
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        show_info(argc);
+    int option, i;
+
+    while ((option = getopt(argc, argv, "dhe:f:c:p:r:mb:")) != -1) {
+        switch (option) {
+        case 'b':
+            bassstring = strdup(optarg);
+            for (char *c=bassstring; *c; c++)
+                *c = tolower(*c);
+            for (i=0; i<BASS_COUNT; i++)
+                if (!strcmp(bassstring, basstypes[i]))
+                    break;
+            if (i==BASS_COUNT) {
+                fprintf(stderr, "invalid bass type\n");
+                return 1;
+            }
+            basstype = i;
+            if (basstype == BASS_SOFTBASS && maxpokvol == DEFAULT_MAXPOKVOL)
+                maxpokvol = 11;             // 12 sometimes distorts
+            break;
+        case 'h':
+        default:
+            usage();
+            return 1;
+            break;
+        }
+    }
+
+    if (optind+1 != argc) {
+        fprintf(stderr, "wrong arguments\n");
+        usage();
         return 1;
     }
 
@@ -25,9 +86,9 @@ int main(int argc, char *argv[]) {
     synth_init(44100);
 
     if (c64SidLoad
-        (argv[1], &init_addr, &play_addr, &actual_subsong, &max_subsong,
+        (argv[optind], &init_addr, &play_addr, &actual_subsong, &max_subsong,
          &speed100Hz, song_name, song_author, song_copyright) == 0) {
-        fprintf(stderr, "Failed to load %s\n", argv[1]);
+        fprintf(stderr, "Failed to load %s\n", argv[optind]);
         return 1;
     }
 
@@ -56,3 +117,6 @@ int main(int argc, char *argv[]) {
 
     return 1;
 }
+
+/* ------------------------------------------------------------------------ */
+
