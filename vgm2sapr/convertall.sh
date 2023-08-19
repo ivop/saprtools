0de0d19ddfd54a12661437b9c241ce456eb35195
Ivo van Poorten <ivopvp@gmail.com>
2023-08-19 07:05:08 +0200

    batch convert all bbc, sg-1000, sms and gg songs

--- vgm2sapr/convertall.sh
+++ vgm2sapr/convertall.sh
@@ -6,6 +6,8 @@ make
 
 #if false; then
 
+# BBC MICRO
+
 ./vgm2sapr bbc/CrazeeRider-title.vgz
 echo -n \
     "Source: BBC Micro                      " \
@@ -41,6 +43,76 @@ echo -n \
 make compress
 make player
 mv player.xex xex/bbc-tubular.xex
+
+# Sega Game 1000
+
+./vgm2sapr sega/'H.E.R.O. (SG) - 02 - Main Theme.vgm'
+echo -n \
+    "Source: Sega Game 1000                 " \
+    "Title : H.E.R.O Main Theme             " \
+    "Author: " > asm/songname.txt
+make compress
+make player60
+mv player.xex xex/sg1k-hero.xex
+
+# Sega Master System
+
+./vgm2sapr sega/'Sonic The Hedgehog - 03 - Green Hill Zone.vgm'
+echo -n \
+    "Source: Sega Game Master System        " \
+    "Title : Sonic - Green Hill Zone        " \
+    "Author: Masato Nakamura, Yuzo Koshiro" > asm/songname.txt
+make compress
+make player60
+mv player.xex xex/sms-sonic-ghz.xex
+
+./vgm2sapr sega/'Ace of Aces - 01 - Title Screen.vgm'
+echo -n \
+    "Source: Sega Game Master System        " \
+    "Title : Ace Of Aces - Title Screen     " \
+    "Author: " > asm/songname.txt
+make compress
+make player60
+mv player.xex xex/sms-aceofaces.xex
+
+./vgm2sapr sega/'system-of-masters.vgz'
+echo -n \
+    "Source: Sega Game Master System        " \
+    "Title : System Of Masters              " \
+    "Author: Alex Mauer" > asm/songname.txt
+make compress
+make player60
+mv player.xex xex/sms-system.xex
+
+# Sega Game Gear
+
+./vgm2sapr sega/'The Incredible Hulk - 01 - Unused Title Theme.vgm'
+echo -n \
+    "Source: Sega Game Gear                 " \
+    "Title : The Incredible Hulk Title Theme" \
+    "Author: Steve Collett, Matt Furniss" > asm/songname.txt
+make compress
+make player60
+mv player.xex xex/gg-hulk.xex
+
+# no framerate in the file
+./vgm2sapr -r 60 sega/'Sonic the Hedgehog 2 - 20 - Good Ending (Game Gear).vgm'
+echo -n \
+    "Source: Sega Game Gear                 " \
+    "Title : Sonic 2 - Good Ending          " \
+    "Author: Masafumi Ogata" > asm/songname.txt
+make compress
+make player60
+mv player.xex xex/gg-sonic2.xex
+
+./vgm2sapr sega/'Spider-Man - X-Men - Arcades Revenge - 01 - Title.vgm'
+echo -n \
+    "Source: Sega Game Gear                 " \
+    "Title : Spider-Man / X-Men: AR         " \
+    "Author: G.Follin, T.Follin, A.Brimble" > asm/songname.txt
+make compress
+make player60
+mv player.xex xex/gg-spider.xex
 
 #fi
 
