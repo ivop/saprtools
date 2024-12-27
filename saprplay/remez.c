719c54e60a30f470b4704171e8c3515554e513c4
Ivo van Poorten <ivopvp@gmail.com>
2024-12-27 06:03:46 +0100

    mzpokeysnd: more credits and proper years, update GPL2 to latest wording

--- saprplay/remez.c
+++ saprplay/remez.c
@@ -2,25 +2,21 @@
  * remez.c - Parks-McClellan algorithm for FIR filter design (C version)
  *
  * Copyright (C) 1995,1998 Jake Janovetz
- * Copyright (C) 1998-2005 Atari800 development team (see DOC/CREDITS)
+ * Copyright (C) 2003-2008 Krzysztof Nikiel, Piotr Fusik, Perry McFarlane
  *
- * This file is part of the Atari800 emulator project which emulates
- * the Atari 400, 800, 800XL, 130XE, and 5200 8-bit computers.
- *
- * Atari800 is free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Atari800 is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Atari800; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
